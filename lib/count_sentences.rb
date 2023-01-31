e3d06ee38e383fc57719d79950a90dba934037ed
Tatiana <Bowmantatiana2@gmail.com>
2023-01-31 10:34:02 -0800

    commit tree 46626e83d527eec93cde0d2c6e06e059717ab145

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+ split_self = self.gsub!(".", ".|").gsub!("!", "!|").split("|")
+ split_self.length
 end
+end
+
+
+
+ "This is a string! It has three sentences. Right?".count_sentences
